18c9d74764b59bf18e8842390e0c960be56f2b26
marcusmellis89 <m.m.ellis89@gmail.com>
2016-01-20 11:27:41 -0800

    serving up static

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 //require the express module
 var express = require('express');
+var path = require('path');
+
+// var bodyParser = require('body-parser');
+// var http = require('http');
+// var request = require('request');
 
 //start up the express object
-vap app = express();
+var app = express();
 
 //declare a port for the server
 var PORT = process.env.PORT || 8000;
+
+// app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'../client')));
 
 //start the server
 app.listen(PORT);
 
 //log to terminal where server will listen 
-console.log("listening on PORT: ", PORT);
+console.log("listening on PORT:", PORT);
